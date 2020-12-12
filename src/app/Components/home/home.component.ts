0688362d17563c3bd8fc5fc66428465cddadf2fa
yosraf <yosrafatnassi6@gmail.com>
2020-12-12 14:32:23 +0100

    adding dynamic categories styling

--- src/app/Components/home/home.component.ts
+++ src/app/Components/home/home.component.ts
@@ -7,6 +7,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
+  categories = [
+    "Meats",
+    "Pasta",
+    "Pizza",
+    "Sandwichs",
+    "Burgers",
+    "Salads",
+    "Grills"
+  ]
   constructor() { }
 
   ngOnInit(): void {
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,9 @@ import { NgModule } from '@angular/core';
 import {MatToolbarModule} from '@angular/material/toolbar';
 import {MatIconModule} from '@angular/material/icon';
 import {MatInputModule} from '@angular/material/input';
+import {MatChipsModule} from '@angular/material/chips';
+import {MatDividerModule} from '@angular/material/divider';
+
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -29,7 +32,9 @@ import { ReactiveFormsModule } from '@angular/forms';
     ReactiveFormsModule,
     MatToolbarModule,
     MatIconModule,
-    MatInputModule
+    MatInputModule,
+    MatChipsModule,
+    MatDividerModule
   ],
   providers: [],
   bootstrap: [AppComponent]
